69a3051761804acc21c4733e593efe72caf60851
Pierre Guilbault <guilbaultpj@gmail.com>
2021-05-11 14:06:16 -0400

    Phase three in progress

--- starwar.js
+++ starwar.js
@@ -4,12 +4,18 @@ const fetch = require('node-fetch');
 console.log(fetch('https://swapi.dev/api/people/1/'));
 
 
-const fetchPpl = fetch('https://swapi.dev/api/people/1', {})
-  .then(res => res.json())
-  .then(json => fetch(json.homeworld), {})
+// const fetchPpl = fetch('https://swapi.dev/api/people/1/', {})
+//   .then(res => res.json())
+//   .then(json => fetchHomeworld(json.homeworld))
 
-const fetchHomeworld = (homeworld) => {
-  fetch(homeworld, {})
-    .then(res => res.json())
-    .then(json => console.log(json))
-}
+// const fetchHomeworld = (homeworld) => {
+//   fetch(homeworld, {})
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+// }
+
+const fetchFilms = () => {fetch("https://swapi.dev/api/people/1/", {})
+  .then((res) => res.json())
+  .then(json =>console.log(json.films))}
+
+fetchFilms()
